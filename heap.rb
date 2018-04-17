daa03c0fd25b19f9e69e203ffedd9445ed9ffe85
appodols <alexander_podolsky@alumni.brown.edu>
2018-04-17 14:51:52 -0400

    heap implemented with private helper methods

--- heap.rb
+++ heap.rb
@@ -1,5 +1,5 @@
 class PriorityQueue
-
+  #this is for a min ordered binary-heap
   attr_reader :heap
 
   def initialize(heap = [nil])
@@ -23,6 +23,40 @@ class PriorityQueue
     min = heap.pop
     bubble_down(1)
     min
+  end
+
+private
+
+  def bubble_up(index)
+    while(index >1 && heap[index/2] > heap[index])
+      exchange(index, index/2)
+      index = index/2
+    end
+  end
+
+
+  def bubble_down(index)
+    while(2*index <= heap.size)
+      j = 2*index
+      if( j < heap.size)
+        j+=1 if heap[j+1] > heap[j]
+      end
+
+      if(heap[index] < j)
+        break
+      end
+
+      exchange(index,j)
+      index = j
+
+    end
+
+
+  end
+
+
+  def exchange(first, second)
+    @heap[first], @heap[second] = @heap[second],@heap[first]
   end
 
 
