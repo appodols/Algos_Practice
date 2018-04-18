093e2039e65af9ee8941604adf18a8675c81e838
appodols <alexander_podolsky@alumni.brown.edu>
2018-04-18 15:55:15 -0400

    inserting into binary search tree with nodes complete

---
+++ binary_tree.rb
@@ -0,0 +1,33 @@
+class Node
+  attr_reader :value
+  attr_accessor :left, :right
+
+  def initialize(value = nil)
+    @value = value
+    @left = nil
+    @right = nil
+  end
+end
+
+class BinarySearchTree
+  attr_accessor :root
+
+  def initialize(value = nil)
+    @root = Node.new(value)
+  end
+
+  def insert(node, value)
+    if node.value == value
+      return node
+    elsif value < node.value
+      insert(node.left, value)
+    elsif value > node.value
+      insert(node.right, value)
+    else
+      return node = Node.new(value)
+    #difference between brown code--brown if the node is less than either of the children
+    #first try inserting on left, then insert on left and call recursively
+    #otherwise try inserting right / adding right
+  end
+
+end
