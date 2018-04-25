f1cbe842dd4a35c39c4761552e8e7963e006a06d
appodols <alexander_podolsky@alumni.brown.edu>
2018-04-25 12:58:09 -0400

    static array

---
+++ static_array.rb
@@ -0,0 +1,25 @@
+class StaticArray
+
+  attr_accessor :store
+
+  def initialize(length)
+    @store = Array.new(length)
+    @size = length
+  end
+
+  def[](index)
+    @store[index]
+  end
+
+
+  def size
+    @size
+  end
+
+  def[]=(index, value)
+    raise "error" if index > @size -1
+    @store[index] = value
+  end
+
+
+end
