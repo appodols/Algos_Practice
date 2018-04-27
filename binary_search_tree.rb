b0bc1fcf36cfeb89402b84a5541122ce23875b2b
appodols <alexander_podolsky@alumni.brown.edu>
2018-04-27 14:40:16 -0400

    insert method written for bsearch tree

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -6,4 +6,40 @@ class Node
     @left = left
     @right = right
   end
+
+  def addLeft(data)
+    to_add = Node.new(data)
+    @left = to_add
+  end
+
+  def addRight(data)
+    to_add = Node.new(data)
+    @right = to_add
+  end
+end
+
+class BST
+  attr_accessor :root
+
+  def initialize(value = nil)
+    @root = Node.new(value)
+  end
+
+  def insert(node, toInsert)
+    if node.data == toInsert
+      return node
+    end
+
+    if toInsert < node.data
+      if(node.left.nil?)
+        node.addLeft(toInsert)
+      else
+        insert(node.left, toInsert)
+      end
+  else
+    if(node.right.nil?)
+      node.addRight(toInsert)
+    else
+      insert(node.right, toInsert)
+    end
 end
