f412ee4af9496a73e127e7367f6131b87c0aa610
appodols <alexander_podolsky@alumni.brown.edu>
2018-05-01 12:26:20 -0400

    insertion into tries

--- trie.rb
+++ trie.rb
@@ -1,10 +1,38 @@
 class Node
 
   attr_accessor :value, :next
-  def initialize(value, next = nil)
+  def initialize(value)
     @value = value
-    @next = next
+    @next = {}
   end
+
+
+end
+
+
+class Trie
+
+  attr_reader :size, :root
+
+    def initialize
+      @size = 0
+      @root = Node.new("")
+    end
+
+    def put(key, value)
+      root = put_helper(root, key, value, 0)
+    end
+
+    def put_helper(node, key, val, d)
+      node = new Node() if(node.nil?)
+      if(d == key.length)
+        x.val = val
+        return node
+      end
+      node.next[c] = put_helper(node.next[c], key, val, d+1)
+      return node;
+    end
+
 
 
 end
