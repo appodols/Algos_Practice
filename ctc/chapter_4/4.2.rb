61055e4775cc79c393f1cc93ddc186e679659821
appodols <alexander_podolsky@alumni.brown.edu>
2018-05-12 13:11:15 -0400

    recursive implementation

--- ctc/chapter_4/4.2.rb
+++ ctc/chapter_4/4.2.rb
@@ -23,3 +23,21 @@ def inserts(arr, bs)
   inserts = minimal_tree_helper(arr)
   inserts.each{|insert|, bs.insert(insert) }
 end
+
+
+def createMinimalBST(arr, start, end)
+  if(end < start)
+    return nil
+  #why is this the recursive condition?
+  #because there is no range
+  mid = (start + end)/2
+  node = Node.new(arr[mid])
+  n.left = createMinimalBST(arr,start,mid-1)
+  n.right = createMinimalBST(arr,mid+1, end)
+  return node
+end
+
+
+
+
+end
