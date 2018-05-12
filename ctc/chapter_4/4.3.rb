51863694da434153c4a99f01dfb03c3ffd6b3e1b
appodols <alexander_podolsky@alumni.brown.edu>
2018-05-12 15:24:14 -0400

    4.3.b

--- ctc/chapter_4/4.3.rb
+++ ctc/chapter_4/4.3.rb
@@ -12,3 +12,26 @@ def create_level_linked_list(root, lists, level)
   create_level_linked_list(root.left, lists, level+1)
   create_level_linked_list(root.right, lists, level+1)
 end
+
+
+
+def createLevelLinkedList(root)
+  result = []
+  current = LinkedList.new
+  if(!root)
+    current.add(root)
+  while(current.size > 0)
+    result.add(current)
+    parents = current
+    current = LinkedList.new
+    parents.each do |parent|
+      if(!parent.left.nil?)
+        current.add(parent.left)
+      end
+      if(!parent.left.nil?)
+        current.add(parent.right)
+      end
+    end
+  end
+  result
+end
